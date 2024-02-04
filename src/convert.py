d9cbbd14ca1c2bffff74c2c4650b9f6d0ef098b4
peixed351 <peixed351@gmail.com>
2024-02-04 14:16:22 +0300

    add settings

--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,14 @@
-import supervisely as sly
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
-from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
 import shutil
+from urllib.parse import unquote, urlparse
 
+import supervisely as sly
+from dataset_tools.convert import unpack_if_archive
+from supervisely.io.fs import file_exists, get_file_name
 from tqdm import tqdm
+
+import src.settings as s
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -29,7 +31,7 @@ def download_dataset(teamfiles_dir: str) -> str:
             total=fsize,
             unit="B",
             unit_scale=True,
-        ) as pbar:        
+        ) as pbar:
             api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
         dataset_path = unpack_if_archive(local_path)
 
@@ -57,7 +59,8 @@ def download_dataset(teamfiles_dir: str) -> str:
 
         dataset_path = storage_dir
     return dataset_path
-    
+
+
 def count_files(path, extension):
     count = 0
     for root, dirs, files in os.walk(path):
@@ -65,21 +68,102 @@ def count_files(path, extension):
             if file.endswith(extension):
                 count += 1
     return count
-    
+
+
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
     ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    images_path = "/home/alex/DATASETS/TODO/Vietnamese_traffic_signs/archive/train_data/images"
+    bboxes_path = "/home/alex/DATASETS/TODO/Vietnamese_traffic_signs/archive/train_data/labels"
+    images_ext = ".png"
+    ann_ext = ".txt"
+    batch_size = 30
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    def create_ann(image_path):
+        labels = []
 
-    # ... some code here ...
+        image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_np.shape[0]
+        img_wight = image_np.shape[1]
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+        bbox_path = os.path.join(curr_bboxes_path, get_file_name(image_path) + ann_ext)
 
-    # return project
+        if file_exists(bbox_path):
+            with open(bbox_path) as f:
+                content = f.read().split("\n")
 
+                for curr_data in content:
+                    if len(curr_data) != 0:
+                        curr_data = list(map(float, curr_data.split(" ")))
+                        obj_class = idx_to_class.get(int(curr_data[0]))
 
+                        left = int((curr_data[1] - curr_data[3] / 2) * img_wight)
+                        right = int((curr_data[1] + curr_data[3] / 2) * img_wight)
+                        top = int((curr_data[2] - curr_data[4] / 2) * img_height)
+                        bottom = int((curr_data[2] + curr_data[4] / 2) * img_height)
+                        rectangle = sly.Rectangle(top=top, left=left, bottom=bottom, right=right)
+                        label = sly.Label(rectangle, obj_class)
+                        labels.append(label)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
+
+    idx_to_class = {
+        0: sly.ObjClass("one way prohibition", sly.Rectangle),
+        1: sly.ObjClass("no parking", sly.Rectangle),
+        2: sly.ObjClass("no stopping and parking", sly.Rectangle),
+        3: sly.ObjClass("no turn left", sly.Rectangle),
+        4: sly.ObjClass("no turn right", sly.Rectangle),
+        5: sly.ObjClass("no u turn", sly.Rectangle),
+        6: sly.ObjClass("no u and left turn", sly.Rectangle),
+        7: sly.ObjClass("no u and right turn", sly.Rectangle),
+        8: sly.ObjClass("no motorbike entry/turning", sly.Rectangle),
+        9: sly.ObjClass("no car entry/turning", sly.Rectangle),
+        10: sly.ObjClass("no truck entry/turning", sly.Rectangle),
+        11: sly.ObjClass("other prohibition", sly.Rectangle),
+        12: sly.ObjClass("indication", sly.Rectangle),
+        13: sly.ObjClass("direction", sly.Rectangle),
+        14: sly.ObjClass("speed limit", sly.Rectangle),
+        15: sly.ObjClass("weight limit", sly.Rectangle),
+        16: sly.ObjClass("height limit", sly.Rectangle),
+        17: sly.ObjClass("pedestrian crossing", sly.Rectangle),
+        18: sly.ObjClass("intersection danger", sly.Rectangle),
+        19: sly.ObjClass("road danger", sly.Rectangle),
+        20: sly.ObjClass("pedestrian danger", sly.Rectangle),
+        21: sly.ObjClass("construction danger", sly.Rectangle),
+        22: sly.ObjClass("slow warning", sly.Rectangle),
+        23: sly.ObjClass("other warning", sly.Rectangle),
+        24: sly.ObjClass("vehicle permission lane", sly.Rectangle),
+        25: sly.ObjClass("vehicle and speed permission lane", sly.Rectangle),
+        26: sly.ObjClass("overpass route", sly.Rectangle),
+        27: sly.ObjClass("no more prohibition", sly.Rectangle),
+        28: sly.ObjClass("other", sly.Rectangle),
+    }
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(obj_classes=list(idx_to_class.values()))
+    api.project.update_meta(project.id, meta.to_json())
+
+    for ds_name in ["train", "val", "test"]:
+        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+        curr_images_path = os.path.join(images_path, ds_name)
+        curr_bboxes_path = os.path.join(bboxes_path, ds_name)
+        images_names = os.listdir(curr_images_path)
+
+        progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
+
+        for images_names_batch in sly.batched(images_names, batch_size=batch_size):
+            img_pathes_batch = [
+                os.path.join(curr_images_path, image_name) for image_name in images_names_batch
+            ]
+
+            img_infos = api.image.upload_paths(dataset.id, images_names_batch, img_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
+
+            anns = [create_ann(image_path) for image_path in img_pathes_batch]
+            api.annotation.upload_anns(img_ids, anns)
+
+            progress.iters_done_report(len(images_names_batch))
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -13,37 +13,41 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "vietnamese traffic signs"
+PROJECT_NAME_FULL: str = "Vietnamese traffic signs detection and recognition dataset"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.Unknown()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Automotive()]
+CATEGORY: Category = Category.SelfDriving()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [AnnotationType.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
-RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
+RELEASE_DATE: Optional[str] = "2023-06-19"  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
     RELEASE_YEAR: int = None
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = (
+    "https://www.kaggle.com/datasets/jaydenguyenx/vietnamese-traffic-signs-detection-and-recognition"
+)
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 13223232
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/vietnamese-traffic-signs"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = (
+    "https://www.kaggle.com/datasets/jaydenguyenx/vietnamese-traffic-signs-detection-and-recognition/download?datasetVersionNumber=1"
+)
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -53,10 +57,10 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # Use dict key to specify name for a button
 PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {"GitHub":"some_link_to_repo_if_exists"}
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
 CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
+AUTHORS: Optional[List[str]] = ["Dat Nguyen"]
 AUTHORS_CONTACTS: Optional[List[str]] = None
 
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
